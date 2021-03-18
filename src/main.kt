e71f99aa342088085525fe641d4281b29b6080dd
Commissar94 <andreykomissarov94@gmail.com>
2021-03-18 12:10:12 +0300

    +ships in flot

--- src/main.kt
+++ src/main.kt
@@ -8,11 +8,13 @@ fun main() {
 
     val flot = Flot(militaryShip1, militaryShip2, passengerShip, passengerShip2, cargoShip)
     flot.synchronizedSailOff()
+    flot.shipsInFlot()
 }
 
 open class Ship() {
     open val speed: Double = 0.0
     open val passengerCapacity: Int = 0
+    open val typeOfShip = ""
 
     open fun sailOff() {
         println("*Ship is silently sailing off*")
@@ -21,6 +23,7 @@ open class Ship() {
     class MilitaryShip : Ship() {
         override val speed: Double = 20.0
         override val passengerCapacity: Int = 100
+        override val typeOfShip: String = "Military Ship"
         val canons = 20
         override fun sailOff() {
             println("*Ship starts shooting to the sky and sail off*")
@@ -30,12 +33,14 @@ open class Ship() {
     class CargoShip : Ship() {
         override val speed: Double = 15.0
         override val passengerCapacity: Int = 20
+        override val typeOfShip: String = "Cargo Ship"
         val cargoCapacity = 100
     }
 
     class PassengerShip : Ship() {
         override val speed: Double = 15.0
         override val passengerCapacity: Int = 300
+        override val typeOfShip: String = "Passenger Ship"
         override fun sailOff() {
             println("*Ship beeps and sail off*")
         }
@@ -56,5 +61,16 @@ class Flot(_ship1: Ship, _ship2: Ship, _ship3: Ship, _ship4: Ship, _ship5: Ship)
         ship3.sailOff()
         ship4.sailOff()
         ship5.sailOff()
+    }
+
+    fun shipsInFlot() {
+        println()
+        println("There are 5 ships in this flot: ")
+        println()
+        println(ship1.typeOfShip)
+        println(ship2.typeOfShip)
+        println(ship3.typeOfShip)
+        println(ship4.typeOfShip)
+        println(ship5.typeOfShip)
     }
 }
